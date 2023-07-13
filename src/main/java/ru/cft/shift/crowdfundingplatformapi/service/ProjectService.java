5e2d95f715cc61f564340839c4788662f6146ce3
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-07-14 01:06:28 +0700

    Добавил спонсирование проекта.

--- src/main/java/ru/cft/shift/crowdfundingplatformapi/service/ProjectService.java
+++ src/main/java/ru/cft/shift/crowdfundingplatformapi/service/ProjectService.java
@@ -6,6 +6,7 @@ import ru.cft.shift.crowdfundingplatformapi.dto.project.FullProjectDto;
 import ru.cft.shift.crowdfundingplatformapi.dto.project.ProjectDto;
 import ru.cft.shift.crowdfundingplatformapi.dto.project.ProjectPagingFilteringSortingRequest;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public interface ProjectService {
@@ -15,4 +16,6 @@ public interface ProjectService {
     PagingResponse<ProjectDto> getPublicProjects(ProjectPagingFilteringSortingRequest dto);
 
     FullProjectDto getPublicProject(UUID projectId);
+
+    FullProjectDto sponsorProject(UUID projectId, BigDecimal money, UUID personId);
 }
